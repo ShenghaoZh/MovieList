305315ca0a9a05349c45c77879b448d7e7784196
ShenghaoZh <shenghao@wustl.edu>
2020-06-02 17:36:31 -0400

    Updates

---
+++ precache-manifest.cfb29a1ffda419b5e5c61568e5258c2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7864d9ca29e36a6d23d40d02cd581e4",
+    "url": "/Spectra-spa/index.html"
+  },
+  {
+    "revision": "a6f3f6f71ca3596687a2",
+    "url": "/Spectra-spa/static/css/main.c429935f.chunk.css"
+  },
+  {
+    "revision": "772b9c8cfe0dd52fe1fc",
+    "url": "/Spectra-spa/static/js/2.95f58057.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Spectra-spa/static/js/2.95f58057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f3f6f71ca3596687a2",
+    "url": "/Spectra-spa/static/js/main.e166771a.chunk.js"
+  },
+  {
+    "revision": "179027a4c0796ce1bc0b",
+    "url": "/Spectra-spa/static/js/runtime-main.0e69d5ee.js"
+  }
+]);
